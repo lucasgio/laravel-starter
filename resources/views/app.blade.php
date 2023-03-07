ec5f081f32d2433de48144a5d462dc8d3f21d0b5
lucasgio <mikaelosoltes@gmail.com>
2023-03-07 15:24:28 -0300

    Project init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>{{ env('APP_NAME', 'RemoteJobs') }}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @vite(['resources/js/app.js','resources/css/app.css'])
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
